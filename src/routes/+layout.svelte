3a961451fade437c0fa97f204398a2953b2d3661
Adam Robinson <adam@mojotech.com>
2024-05-09 10:50:41 -0400

    add top level nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
 	import '../app.css';
+	import Link from '../components/link.svelte';
 </script>
 
-<slot />
+<div class="bg-slate-800 text-white h-screen p-8">
+	<nav class="w-full text-right flex justify-end">
+		<ul class="flex space-x-4">
+			<li>
+				<Link href="/" text="Home" />
+			</li>
+			<li>
+				<Link href="/about" text="About" />
+			</li>
+			<li>
+				<Link href="/contact" text="Contact" />
+			</li>
+		</ul>
+	</nav>
+
+	<slot />
+</div>
