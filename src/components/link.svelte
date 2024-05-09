a4af15e5c8559ed611842dd99bd036cf433857b6
Adam Robinson <adam@mojotech.com>
2024-05-09 14:33:42 -0400

    use slot instead of text prop

--- src/components/link.svelte
+++ src/components/link.svelte
@@ -1,8 +1,7 @@
 <script>
 	export let href = '';
-	export let text = '';
 </script>
 
 <a {href} class="hover:text-slate-200 hover:underline hover:transition-all hover:duration-200">
-	{text}
+	<slot />
 </a>
