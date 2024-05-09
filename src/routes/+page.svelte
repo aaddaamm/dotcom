aa262172cc6fdc1d95ea1b2f6ec4f4603e2dbf21
Adam Robinson <adam@mojotech.com>
2024-05-09 11:27:10 -0400

    styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <div class="p-4 text-slate-200">
-	<h1 class="text-3xl">Adam Robinson</h1>
-	<p>Software Engineer</p>
-
+	<h1 class="text-3xl pb-4">Adam Robinson</h1>
+	<p class="pb-8">Software Engineer</p>
 	<div>
-		<p>
+		<p class="pb-4">
 			Building software fo more than 15 years. I've worked on everything from Insurance Software to
 			commercial power grid planning software.
 		</p>
-		<p>
+		<p class="pb-4">
 			I enjoy gaming and reading fantasy books. I'm a father of 4 and the proud owner of a mini
 			Berna-doodle
 		</p>
