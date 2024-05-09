f9f4f17ec658a02e93c99a7223aaa25f3c1b0dd3
Adam Robinson <adam@mojotech.com>
2024-05-09 11:26:53 -0400

    make and use a button component

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,9 @@
+<script>
+	import Button from '../../components/button.svelte';
+</script>
+
 <div>
 	<h1>Contact</h1>
 	<p>Feel free to reach out to me <a href="mailto:arobinson109@icloud.com">here</a></p>
+	<Button>Click me</Button>
 </div>
