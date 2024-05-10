f2af0a0db46f61706220a8d8df2a74f0f3364441
Adam Robinson <adam@mojotech.com>
2024-05-09 21:34:29 -0400

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.css';
 	import Nav from '../components/nav.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="bg-slate-700 text-white h-screen">
