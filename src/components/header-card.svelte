d82fac065c38fc128efb74de5596bb8f82a6aab8
Adam Robinson <adam@mojotech.com>
2024-05-11 19:17:01 -0400

    move items to the same line

--- src/components/header-card.svelte
+++ src/components/header-card.svelte
@@ -1,5 +1,7 @@
 <div class="flex flex-col text-left">
 	<h1 class="h1">Adam Robinson</h1>
-	<p class="text-sm">Software Engineer</p>
-	<p class="text-xs font-thin">Cranston, RI</p>
+	<div class="flex flex-row items-center">
+		<p class="text-sm mr-1">Software Engineer</p>
+		<p class="text-xs font-thin">Cranston, RI</p>
+	</div>
 </div>
