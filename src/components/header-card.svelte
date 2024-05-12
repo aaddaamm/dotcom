4d9fe33c1d02c533069cc42aa40f9ed4c0bb8451
Adam Robinson <adam@mojotech.com>
2024-05-12 19:17:32 -0400

    fix header card styles

--- src/components/header-card.svelte
+++ src/components/header-card.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col text-left">
 	<h1 class="h1">Adam Robinson</h1>
-	<div class="flex flex-row items-center">
+	<div class="flex flex-row text-center items-center h-full">
 		<p class="text-sm mr-1">Software Engineer</p>
 		<p class="text-xs font-thin">Cranston, RI</p>
 	</div>
