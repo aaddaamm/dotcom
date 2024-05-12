0f341fc2becf67d805d954aae252517ba27787e0
Adam Robinson <adam@mojotech.com>
2024-05-12 11:59:03 -0400

    rewrite homepage to look like a CLI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,50 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { sleep } from '$lib';
+	import { renderLine, renderLines } from '$lib/text';
+
+	const copy = [
+		"Building software for more than 15 years. I've worked on everything from Insurance Software to commercial power grid planning software.",
+		"I enjoy gaming and reading fantasy books. I'm a father of 4 and the proud owner of a mini Berna-doodle"
+	];
+
+	onMount(async () => {
+		await sleep(2000);
+
+		const commandRenderTarget = document.getElementById('command') || document.createElement('div');
+
+		const classes = 'inline pb-4';
+
+		renderLine('about', commandRenderTarget, classes);
+
+		await sleep(2000);
+
+		commandRenderTarget.remove();
+
+		const copyRenderTarget =
+			document.getElementById('copy-render-target') || document.createElement('div');
+
+		renderLines(copy, copyRenderTarget);
+	});
+	// i want the cursor to blink for about 5 seconds then
+	// i want to display some text about me and my hobbies
+</script>
+
 <div>
-	<p class="pb-4">
-		Building software fo more than 15 years. I've worked on everything from Insurance Software to
-		commercial power grid planning software.
-	</p>
-	<p class="pb-4">
-		I enjoy gaming and reading fantasy books. I'm a father of 4 and the proud owner of a mini
-		Berna-doodle
-	</p>
+	<div id="copy-render-target" />
+	<span>{'>'}</span>
+	<span id="command" />
+	<span class="blinking-cursor">|</span>
 </div>
+
+<style>
+	.blinking-cursor {
+		animation: blink 1s steps(2, start) infinite;
+	}
+
+	@keyframes blink {
+		to {
+			visibility: hidden;
+		}
+	}
+</style>
