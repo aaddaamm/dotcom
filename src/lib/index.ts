3b1bc8e175ab2de8b986b7ad5af6df29896ab469
Adam Robinson <adam@mojotech.com>
2024-05-13 22:46:47 -0400

    add types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-export function sleep(ms: number) {
+export function sleep(ms: number): Promise<void> {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { dev } from '$app/environment';
 	import Header from '../components/header.svelte';
