756e33511c26fa4f8fee0a2fac8580fbdaef0957
Adam Robinson <adam@mojotech.com>
2024-05-14 14:44:26 -0400

    update Experience Copy

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,4 +1,24 @@
 <div>
 	<h2 class="h2">Experience</h2>
 	<p class="pb-8">I'm a software engineer who enjoys Reading and Gaming</p>
+	<h3 class="h3">The Dark Times</h3>
+	<p class="pb-8">
+		Worked in many different industries. I've worked in the food industry, the retail industry, the
+		construction industry, sales on a warehouse team, warehouse receiving/shipping, and a few
+		others.
+	</p>
+	<h3 class="h3">Beacon Mutual</h3>
+	<p class="pb-8">
+		I worked at Beacon Mutual for 4 years. I started off working as a Production systems controller.
+		My duties were mostly administering file transfers, code deploys, environment process management
+		and generation. I was then promoted to a software engineer where I mostly worked on the Database
+		and stored procedures using PL/SQL and a very costom/neich Javascript library designed
+		specifically for this company's systems. Very specifically.
+	</p>
+	<h3 class="h3">MojoTech</h3>
+	<p class="pb-8">
+		I am currently working for MojoTech. I am a full stack engineer working on a variety of
+		projects. i've worked on everything from investment platforms, insurance software, a healthcare
+		knowledge sharing platform, a commercial power grid planning software, and a few other things.
+	</p>
 </div>
