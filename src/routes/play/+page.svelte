684d7fd07c8af241c0a96137e1c1a8f28693587c
Adam Robinson <adam@mojotech.com>
2024-05-21 15:28:09 -0400

    music - wip

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Books from './books.svelte';
+	import Music from './music.svelte';
 </script>
 
 <div>
 	<h2 class="h2 pb-8">What about the other stuff?</h2>
 	<Books />
+	<Music />
 </div>
---
+++ src/routes/play/music.svelte
@@ -0,0 +1,6 @@
+<div>
+	<h3 class="h3 pb-8">Music</h3>
+	<p class="pb-4">
+		I decided to play around with apple music API as that is the music service i use.
+	</p>
+</div>
