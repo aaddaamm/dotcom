f9221a444188c432ca86f4e1ea49209af21bdce2
Adam Robinson <adam@mojotech.com>
2024-05-24 22:02:32 -0400

    update goodreads helpers

--- src/lib/goodreads-helpers.ts
+++ src/lib/goodreads-helpers.ts
@@ -1,28 +1,21 @@
 import type { Element } from 'cheerio';
 import { load } from 'cheerio';
 
-// Usage: import { parseCoverFromHTML } from './lib/goodreads-helpers';
 export function parseCoverFromHTML(element: Element) {
-	const img = load(element)('img').attr('src');
-	return img;
+	return load(element)('img').attr('src');
 }
 
 export function parseTitleAndSeriesFromHTML(element: Element) {
 	const markup = load(element)('.title a').text().trim();
-
 	const [title, series] = markup.split('\n');
-
 	return { title, series };
 }
 
 export function parseAuthorFromHTML(element: Element) {
-	const author = load(element)('.author a').text().trim();
-
-	return author;
+	return load(element)('.author a').text().trim();
 }
 
 export function parseGoodreadsURLFromHTML(element: Element) {
 	const url = load(element)('.title a').attr('href');
-
 	return `https://www.goodreads.com${url}`;
 }
