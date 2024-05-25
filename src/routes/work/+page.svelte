50cff890169b02d2e8e527cc871d65a0be55e3d4
Adam Robinson <adam@mojotech.com>
2024-05-24 22:48:41 -0400

    add blinking cursor to work page

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { renderElements } from '$lib/text';
+	import { sleep } from '$lib';
+	import { renderElements, renderElement } from '$lib/text';
 	import { workCopy } from '$lib/copy';
+	import type { Element } from '$lib/types';
 
 	onMount(async () => {
+		const commandRenderTarget = document.getElementById('command') || document.createElement('div');
+		const element: Element = {
+			type: 'span',
+			children: 'site --work',
+			props: {
+				className: 'inline pb-4'
+			}
+		};
+
+		await renderElement(element, commandRenderTarget);
+
+		await sleep(200);
+
+		// remove the command text
+		commandRenderTarget.remove();
+
 		const workRenderTarget =
 			document.getElementById('work-render-target') || document.createElement('div');
 		await renderElements(workCopy, workRenderTarget);
@@ -12,4 +30,19 @@
 
 <div>
 	<div id="work-render-target" />
+	<span>&gt;</span>
+	<span id="command" />
+	<span class="blinking-cursor">|</span>
 </div>
+
+<style>
+	.blinking-cursor {
+		animation: blink 1s steps(2, start) infinite;
+	}
+
+	@keyframes blink {
+		to {
+			visibility: hidden;
+		}
+	}
+</style>
