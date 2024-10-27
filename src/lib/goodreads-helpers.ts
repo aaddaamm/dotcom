4fbb3630a4ea8a10029a979e4e027c6350f65b30
Adam Robinson <adam@mojotech.com>
2024-10-26 22:07:53 -0400

    increase image assets URLs to use larger assets

--- src/lib/goodreads-helpers.ts
+++ src/lib/goodreads-helpers.ts
@@ -1,23 +1,42 @@
-import type { Element } from 'cheerio';
-import { load } from 'cheerio';
+import * as cheerio from 'cheerio';
 
-export function parseCoverFromHTML(element: Element) {
-	const url = load(element)('img').attr('src');
+// https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1670363463i/58416952.jpg
+// https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654573897i/34002132.jpg
 
-	return url;
+export function useLargerImageAssets(imageURL?: string) {
+	if (!imageURL) return '';
+
+	const newOrigin = 'https://images-na.ssl-images-amazon.com';
+
+	// Replace origin and remove part between underscores
+	const updatedUrl = imageURL.replace(/^https:\/\/[^\/]+/, newOrigin).replace(/._\w+_/g, '');
+
+	return updatedUrl;
 }
 
-export function parseTitleAndSeriesFromHTML(element: Element) {
-	const markup = load(element)('.title a').text().trim();
+export function parseCoverFromHTML(element: cheerio.Element) {
+	const imageURL = cheerio.load(element)('img').attr('src');
+
+	const updatedURL = useLargerImageAssets(imageURL);
+
+	return updatedURL;
+}
+
+export function parseTitleAndSeriesFromHTML(element: cheerio.Element) {
+	const markup = cheerio.load(element)('.title a').text().trim();
 	const [title, series] = markup.split('\n');
+
 	return { title, series };
 }
 
-export function parseAuthorFromHTML(element: Element) {
-	return load(element)('.author a').text().trim();
+export function parseAuthorFromHTML(element: cheerio.Element) {
+	const author = cheerio.load(element)('.author a').text().trim();
+
+	return author;
 }
 
-export function parseGoodreadsURLFromHTML(element: Element) {
-	const url = load(element)('.title a').attr('href');
-	return `https://www.goodreads.com${url}`;
+export function parseGoodreadsURLFromHTML(element: cheerio.Element) {
+	const goodreadsURL = cheerio.load(element)('.title a').attr('href');
+
+	return `https://www.goodreads.com${goodreadsURL}`;
 }
