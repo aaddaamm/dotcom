420daed3d9f48beff582dead87f45efc8e25bf9e
Adam Robinson <adam@mojotech.com>
2024-11-13 18:49:34 -0500

    use new helper to parse book

--- src/routes/api/goodreads/currently-reading/+server.ts
+++ src/routes/api/goodreads/currently-reading/+server.ts
@@ -1,30 +1,22 @@
-import {
-	parseCoverFromHTML,
-	parseTitleAndSeriesFromHTML,
-	parseAuthorFromHTML,
-	parseGoodreadsURLFromHTML
-} from '$lib/goodreads/helpers';
+import { parseBookFromElement } from '$lib/goodreads/helpers';
 import axios from 'axios';
 import { load } from 'cheerio';
 import { type GoodreadsBook } from '$lib/types';
 import { GOODREADS_SHELVES } from '$lib/constants';
 
-const BASE_URL = 'https://www.goodreads.com/review/list/92024399-adam-robinson';
+export const _BASE_URL = 'https://www.goodreads.com/review/list/92024399-adam-robinson';
 
 export async function GET() {
-	const resp = await axios(`${BASE_URL}?shelf=${GOODREADS_SHELVES.CURRENTLY_READING}`);
+	const resp = await axios(`${_BASE_URL}?shelf=${GOODREADS_SHELVES.CURRENTLY_READING}`);
 
 	const html = load(resp.data);
 
 	const books: GoodreadsBook[] = [];
 
 	html('tr.bookalike').each((i, elem) => {
-		const cover = parseCoverFromHTML(elem);
-		const { title, series } = parseTitleAndSeriesFromHTML(elem);
-		const author = parseAuthorFromHTML(elem);
-		const url = parseGoodreadsURLFromHTML(elem);
+		const book = parseBookFromElement(elem);
 
-		books.push({ cover, title, series, author, url });
+		books.push(book);
 	});
 
 	return new Response(JSON.stringify(books), {
