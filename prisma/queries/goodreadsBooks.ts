405c80c6afd75c859a5ace6052be07965421d2f8
Adam Robinson <adam@mojotech.com>
2024-11-17 20:24:52 -0500

    add goodreads queries file

---
+++ prisma/queries/goodreadsBooks.ts
@@ -0,0 +1,25 @@
+import client from '../client';
+import type { GoodreadsBook } from '../../src/lib/types';
+
+export async function addGoodreadsBook(goodreadsData: GoodreadsBook) {
+	const { cover, title, series, author, url, rating, dateRead, isbn, isbn13, asin, goodreadsID } =
+		goodreadsData;
+
+	const goodreadsBook = await client.goodreadsBook.create({
+		data: {
+			imageURL: cover,
+			goodreadsId: goodreadsID,
+			title,
+			series,
+			authorName: author,
+			rating,
+			url,
+			dateRead: dateRead ? new Date(dateRead) : null,
+			isbn,
+			isbn13,
+			asin
+		}
+	});
+
+	return goodreadsBook;
+}
